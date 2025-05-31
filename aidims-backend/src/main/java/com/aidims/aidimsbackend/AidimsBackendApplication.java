4da9e91c3af513f745102d6a464cbdb8b2995cae
Lê Minh Huy <minhhuyle456@gmail.com>
2025-05-31 14:14:59 +0700

    add new

---
+++ aidims-backend/src/main/java/com/aidims/aidimsbackend/AidimsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.aidims.aidimsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AidimsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AidimsBackendApplication.class, args);
+    }
+
+}
---
+++ aidims-backend/src/test/java/com/aidims/aidimsbackend/AidimsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aidims.aidimsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AidimsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
