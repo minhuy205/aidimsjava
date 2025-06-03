118f796a201af49f418c240796eebc6bf569ea73
Lê Minh Huy <minhhuyle456@gmail.com>
2025-06-03 21:03:02 +0700

    add lai trang chu cho KC va fix loi tao thu muc moi

--- aidims-frontend/src/index.js
+++ aidims-frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import HomePage from "./pages/Guest/homepage"
+import './css/index.css';
+import HomePage from "./pages/Guest/index.js";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<HomePage />);
