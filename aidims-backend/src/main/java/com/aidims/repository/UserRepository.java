4023613701fd8553313b334a9087edac303fed21
Gia Nguyễn Trần Huỳnh <gianguyen150605@gmail.com>
2025-06-11 16:52:27 +0700

    Backend Doctor

--- aidims-backend/src/main/java/com/aidims/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-// UserRepository.java
-package com.aidims.repository;
-
-import com.aidims.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
